0071fe9e62501e240eb9f08c17e5511ceebe83af
sinanex <ms4730435@gmail.com>
2025-02-10 10:36:49 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:slms/views/login/login_screen.dart';
 
 void main() {
+    WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
@@ -10,7 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Color(0xff156EF6)),
+      
+      home: LoginScreen(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(
+        ),
+        primaryColor: Color(0xff156EF6)),
     );
   }
 }
--- lib/views/login/login_screen.dart
+++ lib/views/login/login_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:slms/utils/color/color.dart';
+import 'package:slms/utils/image/image.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: ColorConstents.bagroundColor,
+      body: Center(
+        child: Column(
+          children: [
+             Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+               children: [
+                 Image.asset(ImageConstents.bridgeonLogo,width: 50,),
+                 Column(
+                   children: [
+                     Text("Bridgeon",style: TextStyle(
+                      fontWeight: FontWeight.w900,fontSize: 20,
+                     ),),
+                     Text("code you future")
+                   ],
+                 ),
+               ],
+             ),
+          ],
+        ),
+      ),
+    );
   }
 }
