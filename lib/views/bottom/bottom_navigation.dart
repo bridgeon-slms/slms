1a2ce6d1b01736606d0100e431fb50261582d19f
Muhammad Sinan <ms4730435@gmail.com>
2025-02-13 13:50:43 +0530

    updated

--- lib/views/bottom/bottom_navigation.dart
+++ lib/views/bottom/bottom_navigation.dart
@@ -5,7 +5,6 @@ import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 import 'package:slms/utils/color/color.dart';
 import 'package:slms/view_model/bottom_bar.dart';
 import 'package:slms/views/%20AttendancePage/%20AttendanceMain/%20attendance.dart';
-import 'package:slms/views/ProfilePage/profilepage.dart';
 import 'package:slms/views/home/home_screen.dart';
 import 'package:slms/views/payments/payment_screen.dart';
 import 'package:slms/views/reviews/score_details.dart';
