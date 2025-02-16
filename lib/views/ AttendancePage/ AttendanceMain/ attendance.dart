ec44828ec664a080bc1c69f62f69bb4a0ac7dfec
Rinshid10 <rinshidch10@gmail.com>
2025-02-16 10:20:20 +0530

    with functionality

--- lib/views/ AttendancePage/ AttendanceMain/ attendance.dart
+++ lib/views/ AttendancePage/ AttendanceMain/ attendance.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:provider/provider.dart';
+import 'package:slms/AttendendsServices/attendencecontroller.dart';
 import 'package:slms/utils/color/color.dart';
 import 'package:slms/views/%20AttendancePage/%20AttendanceReport/chartpage.dart';
 import 'package:slms/views/%20AttendancePage/%20AttendanceMain/widgets.dart';
 import 'package:slms/widget/widget.dart';
 
-class AttendancePage extends StatelessWidget {
+class AttendancePage extends StatefulWidget {
   const AttendancePage({super.key});
+
+  @override
+  State<AttendancePage> createState() => _AttendancePageState();
+}
+
+class _AttendancePageState extends State<AttendancePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Provider.of<Attendencecontroller>(context, listen: false)
+        .getAllDataFromAttendence();
+    Provider.of<Attendencecontroller>(context, listen: false)
+        .getAllDataFromAttendenceLog();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,114 +34,124 @@ class AttendancePage extends StatelessWidget {
         body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(15),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                textStyled(text: 'Hi Rinshid'),
-                textStyled(
-                    text: 'Attendance Report',
-                    fontweight: FontWeight.bold,
-                    fontSize: 18),
-                Gap(20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+            child: Consumer<Attendencecontroller>(
+              builder: (context, datas, child) {
+                if (datas.attendenceList.isEmpty) {
+                  return Center(child: CircularProgressIndicator());
+                }
+
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Gap(10),
-                    dateContainer(
-                      ontap: () async {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(16),
-                              ),
-                              title: Column(
-                                children: [
-                                  Text(
-                                    "Select The Date",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18,
-                                      color: Colors.black87,
-                                    ),
+                    textStyled(
+                        text: 'Hi ${datas.attendenceList.first.userId?.name}'),
+                    textStyled(
+                        text: 'Attendance Report',
+                        fontweight: FontWeight.bold,
+                        fontSize: 18),
+                    Gap(20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Gap(10),
+                        dateContainer(
+                          ontap: () async {
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(16),
                                   ),
-                                  Divider(thickness: 1),
-                                ],
-                              ),
-                              content: SizedBox(
-                                width: 320,
-                                height: 350,
-                                child: SingleChildScrollView(
-                                  child: Column(
+                                  title: Column(
                                     children: [
-                                      filterDateButton("Show all",
-                                          Icons.calendar_today, () {}),
-                                      filterDateButton("Last 7 days",
-                                          Icons.date_range, () {}),
-                                      filterDateButton("Last 30 days",
-                                          Icons.event_note, () {}),
-                                      filterDateButton(
-                                          "Today", Icons.today, () {}),
-                                      filterDateButton(
-                                          "Yesterday", Icons.history, () {}),
-                                      filterDateButton(
-                                          "This Week", Icons.view_week, () {}),
-                                      filterDateButton(
-                                          "Last Week", Icons.arrow_back, () {}),
-                                      filterDateButton("This Month",
-                                          Icons.calendar_month, () {}),
-                                      filterDateButton("Last Month",
-                                          Icons.calendar_view_month, () {}),
+                                      Text(
+                                        "Select The Date",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18,
+                                          color: Colors.black87,
+                                        ),
+                                      ),
+                                      Divider(thickness: 1),
                                     ],
                                   ),
-                                ),
-                              ),
-                              actions: [
-                                TextButton(
-                                  onPressed: () => Navigator.of(context).pop(),
-                                  child: Text(
-                                    "CLOSE",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.red,
+                                  content: SizedBox(
+                                    width: 320,
+                                    height: 350,
+                                    child: SingleChildScrollView(
+                                      child: Column(
+                                        children: [
+                                          filterDateButton("Show all",
+                                              Icons.calendar_today, () {}),
+                                          filterDateButton("Last 7 days",
+                                              Icons.date_range, () {}),
+                                          filterDateButton("Last 30 days",
+                                              Icons.event_note, () {}),
+                                          filterDateButton(
+                                              "Today", Icons.today, () {}),
+                                          filterDateButton("Yesterday",
+                                              Icons.history, () {}),
+                                          filterDateButton("This Week",
+                                              Icons.view_week, () {}),
+                                          filterDateButton("Last Week",
+                                              Icons.arrow_back, () {}),
+                                          filterDateButton("This Month",
+                                              Icons.calendar_month, () {}),
+                                          filterDateButton("Last Month",
+                                              Icons.calendar_view_month, () {}),
+                                        ],
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () =>
+                                          Navigator.of(context).pop(),
+                                      child: Text(
+                                        "CLOSE",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.red,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              },
                             );
                           },
-                        );
-                      },
-                    )
-                  ],
-                ),
-                Gap(10),
-                TabBar(
-                  labelColor: ColorConstents.primeryColor,
-                  unselectedLabelColor: Colors.grey,
-                  indicatorColor: ColorConstents.primeryColor,
-                  tabs: [
-                    Tab(
-                      icon: Icon(Icons.list),
+                        )
+                      ],
                     ),
-                    Tab(
-                      icon: Icon(Icons.incomplete_circle),
+                    Gap(10),
+                    TabBar(
+                      labelColor: ColorConstents.primeryColor,
+                      unselectedLabelColor: Colors.grey,
+                      indicatorColor: ColorConstents.primeryColor,
+                      tabs: [
+                        Tab(
+                          icon: Icon(Icons.list),
+                        ),
+                        Tab(
+                          icon: Icon(Icons.incomplete_circle),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                Gap(20),
-                Expanded(
-                  child: TabBarView(
-                    children: [
-                      attenceBar(context: context),
-                      Center(
-                        child: Dashboard(),
+                    Gap(20),
+                    Expanded(
+                      child: TabBarView(
+                        children: [
+                          attenceBar(context: context),
+                          Center(
+                            child: Dashboard(),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-              ],
+                    ),
+                  ],
+                );
+              },
             ),
           ),
         ),
